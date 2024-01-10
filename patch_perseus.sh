17853d9b0d4393bef7cdf8df1e07cb2fe2b7615e
restiabot <tanpaid1911@gmail.com>
2024-01-10 07:16:59 +0700

    Update patch_perseus.sh

--- patch_perseus.sh
+++ patch_perseus.sh
@@ -57,7 +57,7 @@ echo "Copy Perseus libs"
 cp -r Perseus/. com.manjuu.azurlane.inner/lib/
 
 echo "Patching Azur Lane with Perseus"
-oncreate=$(grep -n -m 1 'onCreate' com.manjuu.azurlane.innersmali_classes2/com/unity3d/player/UnityPlayerActivity.smali | sed  's/[0-9]*\:\(.*\)/\1/')
+oncreate=$(grep -n -m 1 'onCreate' com.manjuu.azurlane.inner/smali_classes2/com/unity3d/player/UnityPlayerActivity.smali | sed  's/[0-9]*\:\(.*\)/\1/')
 sed -ir "s#\($oncreate\)#.method private static native init(Landroid/content/Context;)V\n.end method\n\n\1#" com.manjuu.azurlane.inner/smali_classes2/com/unity3d/player/UnityPlayerActivity.smali
 sed -ir "s#\($oncreate\)#\1\n    const-string v0, \"Perseus\"\n\n\    invoke-static {v0}, Ljava/lang/System;->loadLibrary(Ljava/lang/String;)V\n\n    invoke-static {p0}, Lcom/unity3d/player/UnityPlayerActivity;->init(Landroid/content/Context;)V\n#" com.manjuu.azurlane.inner/smali_classes2/com/unity3d/player/UnityPlayerActivity.smali
 
